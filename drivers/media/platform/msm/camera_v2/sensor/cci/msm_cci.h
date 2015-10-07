ddf442239138eead2ef9c33a7eef4642c5d48b63
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-10-07 08:56:47 +0800

    msm: camera: Revert to LNX.LA.3.7.2 stack
    Change-Id: I0020834634135091d0040eb2509f164af9d85d17

--- drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.h
+++ drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.h
@@ -20,6 +20,11 @@
 #include <media/msm_cam_sensor.h>
 #include <soc/qcom/camera2.h>
 #include "msm_sd.h"
+/*Added by Jinshui.Liu@Camera 20140221 start for cci error*/
+#ifdef CONFIG_MACH_HM2014811
+#include <linux/wakelock.h>
+#endif
+/*Added by Jinshui.Liu@Camera 20140221 end*/
 
 #define NUM_MASTERS 2
 #define NUM_QUEUES 2
@@ -29,8 +34,6 @@
 
 #define CCI_PINCTRL_STATE_DEFAULT "cci_default"
 #define CCI_PINCTRL_STATE_SLEEP "cci_suspend"
-
-#define CCI_NUM_CLK_MAX	16
 
 enum cci_i2c_queue_t {
 	QUEUE_0,
@@ -135,7 +138,7 @@ struct cci_device {
 	enum msm_cci_state_t cci_state;
 	uint32_t num_clk;
 
-	struct clk *cci_clk[CCI_NUM_CLK_MAX];
+	struct clk *cci_clk[5];
 	struct msm_camera_cci_i2c_queue_info
 		cci_i2c_queue_info[NUM_MASTERS][NUM_QUEUES];
 	struct msm_camera_cci_master_info cci_master_info[NUM_MASTERS];
@@ -145,8 +148,11 @@ struct cci_device {
 	uint8_t master_clk_init[MASTER_MAX];
 	struct msm_pinctrl_info cci_pinctrl;
 	uint8_t cci_pinctrl_status;
-	struct regulator *reg_ptr;
-	uint32_t cycles_per_us;
+/*Added by Jinshui.Liu@Camera 20140221 start for cci error*/
+#ifdef CONFIG_MACH_HM2014811
+	struct wake_lock cci_wakelock;
+#endif
+/*Added by Jinshui.Liu@Camera 20140221 end*/
 };
 
 enum msm_cci_i2c_cmd_type {
@@ -182,13 +188,7 @@ enum msm_cci_gpio_cmd_type {
 	CCI_GPIO_INVALID_CMD,
 };
 
-#ifdef CONFIG_MSM_CCI
 struct v4l2_subdev *msm_cci_get_subdev(void);
-#else
-static inline struct v4l2_subdev *msm_cci_get_subdev(void) {
-	return NULL;
-}
-#endif
 
 #define VIDIOC_MSM_CCI_CFG \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 23, struct msm_camera_cci_ctrl *)
